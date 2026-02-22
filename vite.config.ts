873ca062505173f858a9a06390fb93d718709058
Auri Gabriel <ririmello.a@gmail.com>
2026-02-21 22:47:16 -0300

    feat: enhance Vite configuration with build and CSS options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/quadro-de-avaliacao/',
+  optimizeDeps: {
+    include: ['react-quill-new'],
+  },
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'mixed-decls',
+          'color-functions',
+          'global-builtin',
+        ],
+      },
+    },
+  },
+});
