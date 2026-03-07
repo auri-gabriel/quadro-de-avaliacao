020f1a7b279a1abcc6b9eb5f7b5f4ad641af0369
Auri Gabriel <ririmello.a@gmail.com>
2026-03-07 14:50:30 -0300

    feat: update AppFooter with new copyright and repository link

--- src/shared/ui/AppFooter.tsx
+++ src/shared/ui/AppFooter.tsx
@@ -1,28 +1,18 @@
 export function AppFooter() {
   const repositoryUrl = 'https://github.com/auri-gabriel/quadro-de-avaliacao';
-  const issuesUrl = `${repositoryUrl}/issues`;
 
   return (
     <footer className='app-footer bg-dark text-white text-center py-5'>
-      <div className='container-fluid board-layout'>
-        <small>
-          © 2025 TRAMAS. Todos os direitos reservados.{' '}
+      <div className='container'>
+        <small className='d-flex flex-column gap-1'>
+          <span>© 2025 GEInfoEdu.</span>
           <a
             href={repositoryUrl}
             target='_blank'
             rel='noreferrer'
             className='link-light'
           >
-            Repositório
-          </a>{' '}
-          |{' '}
-          <a
-            href={issuesUrl}
-            target='_blank'
-            rel='noreferrer'
-            className='link-light'
-          >
-            Reportar problema
+            Código-fonte no GitHub
           </a>
         </small>
       </div>
